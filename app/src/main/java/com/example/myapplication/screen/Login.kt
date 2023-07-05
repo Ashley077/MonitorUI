51830421409f6b9e235c2d35d938ce61dbfae5f2
Ashley <m910620123@gmail.com>
2023-07-05 18:05:34 +0800

    登入狀態服務

--- app/src/main/java/com/example/myapplication/screen/Login.kt
+++ app/src/main/java/com/example/myapplication/screen/Login.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication.screen
 
-import android.widget.Toast
+//import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
@@ -11,9 +11,6 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-//import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,27 +18,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.myapplication.Routes
-import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.ui.theme.Purple700
-import com.example.myapplication.viewmodel.LoginViewModel
 import com.example.myapplication.viewmodel.AccountViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.example.myapplication.viewmodel.LoginViewModel
+import com.example.myapplication.viewmodel.RemoteLoginStatus
 
-@OptIn(ExperimentalStdlibApi::class)
 @Composable
-fun LoginPage(navController: NavHostController,accountViewModel:AccountViewModel,loginviewModel: LoginViewModel) {
+fun LoginPage(
+    navController: NavHostController,
+    accountViewModel: AccountViewModel,
+    loginViewModel: LoginViewModel
+) {
     val inputUserName = accountViewModel.inputUserNameData.observeAsState()
     val inputPassword = accountViewModel.inputPasswordData.observeAsState()
     val canButtonEnable = accountViewModel.canLoginButtonEnableData.observeAsState()
@@ -49,105 +43,117 @@ fun LoginPage(navController: NavHostController,accountViewModel:AccountViewModel
 
     val coroutineScope = rememberCoroutineScope()
 
-    val allUser = loginviewModel.allUser.observeAsState()
+    val allUser = loginViewModel.allUser.observeAsState()
 
-    val isLoading = remember { mutableStateOf(false) }
-    if(isLoading.value){
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator(
-                color = Color.Red
-            )
-        }
-
-
-    }
-    else{
-        Box(modifier = Modifier.fillMaxSize()) {
-            ClickableText(
-                text = AnnotatedString("Sign up here"),
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(20.dp),
-                onClick = { navController.navigate(Routes.SignUp.route)},
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    fontFamily = FontFamily.Default,
-                    textDecoration = TextDecoration.Underline,
-                    color = Purple700
+    val status = loginViewModel.status.observeAsState()
+    when (status.value) {
+        RemoteLoginStatus.Loading -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    color = Color.Red
                 )
-            )
-        }
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-
-            Text(text = "Login", style = TextStyle(fontSize = 50.sp,fontFamily = FontFamily.Cursive))
-
-            Spacer(modifier = Modifier.height(20.dp))
-            TextField(
-                label = { Text(text = "Username") },
-                value = inputUserName.value ?: "" ,
-                onValueChange = { accountViewModel.setInputUserName(it)}
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
-            TextField(
-                label = { Text(text = "Password") },
-                value = inputPassword.value ?: "" ,
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                onValueChange = { accountViewModel.setInputPassword(it) })
-
-            Spacer(modifier = Modifier.height(20.dp))
-            Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
-                Button(modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-
-                    onClick = {
-                        isLoading.value = true
-                        coroutineScope.launch(Dispatchers.Main) {
-                            val isSuccess: Boolean = accountViewModel.login()
-                            if (isSuccess) {
-                                navController.navigate(Routes.Voice.route)
-                            } else {
-                                isLoading.value = false
-                            }
-                        }
-                        loginviewModel.addUser(inputUserName.value ?: "", inputPassword.value ?: "")
-                       },
-                    enabled = canButtonEnable.value ?: false,
-                    shape = RoundedCornerShape(50.dp),
-
-                ) {
-                    Text(text = "Login" )
-                }
             }
-            if (lastTimeLoginIsFail.value!!) {
-                Box(modifier = Modifier.padding(40.dp, 10.dp, 40.dp, 0.dp)) {
-                    Text(
-                        text = "帳號密碼錯誤",
-                        fontSize = 25.sp,
-                        color = Color.Red
+        }
+        is RemoteLoginStatus.Success -> {
+            if (navController.currentBackStackEntry?.destination?.route == Routes.Login.route) {
+                navController.navigate(Routes.Voice.route)
+            }
+        }
+        else -> {
+            Box(modifier = Modifier.fillMaxSize()) {
+                ClickableText(
+                    text = AnnotatedString("Sign up here"),
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(20.dp),
+                    onClick = { navController.navigate(Routes.SignUp.route) },
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily.Default,
+                        textDecoration = TextDecoration.Underline,
+                        color = Purple700
                     )
-                }
-            }
-            Spacer(modifier = Modifier.height(20.dp))
-            ClickableText(
-                text = AnnotatedString("Forgot password?"),
-                onClick = { navController.navigate(Routes.ForgotPassword.route) },
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    fontFamily = FontFamily.Default
                 )
-            )
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+
+                Text(
+                    text = "Login",
+                    style = TextStyle(fontSize = 50.sp, fontFamily = FontFamily.Cursive)
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+                TextField(
+                    label = { Text(text = "Username") },
+                    value = inputUserName.value ?: "",
+                    onValueChange = { accountViewModel.setInputUserName(it) }
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+                TextField(
+                    label = { Text(text = "Password") },
+                    value = inputPassword.value ?: "",
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    onValueChange = { accountViewModel.setInputPassword(it) })
+
+                Spacer(modifier = Modifier.height(20.dp))
+                Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp),
+
+                        onClick = {
+                            loginViewModel.login(
+                                inputUserName.value ?: "",
+                                inputPassword.value ?: ""
+                            )
+
+//                        coroutineScope.launch(Dispatchers.Main) {
+//                            val isSuccess: Boolean = accountViewModel.login()
+//                            if (isSuccess) {
+//                                navController.navigate(Routes.Voice.route)
+//                            } else {
+//
+//                            }
+//                        }
+//                        loginviewModel.addUser(inputUserName.value ?: "", inputPassword.value ?: "")
+                        },
+                        enabled = canButtonEnable.value ?: false,
+                        shape = RoundedCornerShape(50.dp),
+
+                        ) {
+                        Text(text = "Login")
+                    }
+                }
+                if (status.value is RemoteLoginStatus.Failed) {
+                    Box(modifier = Modifier.padding(40.dp, 10.dp, 40.dp, 0.dp)) {
+                        Text(
+                            text = "帳號密碼錯誤",
+                            fontSize = 25.sp,
+                            color = Color.Red
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                ClickableText(
+                    text = AnnotatedString("Forgot password?"),
+                    onClick = { navController.navigate(Routes.ForgotPassword.route) },
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily.Default
+                    )
+                )
+            }
         }
     }
-
 }
--- app/src/main/java/com/example/myapplication/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/LoginViewModel.kt
@@ -1,25 +1,91 @@
 package com.example.myapplication.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.myapplication.manager.RemoteClientManager
 import com.example.myapplication.model.data.local.dao.UserInfoDao
-import com.example.myapplication.model.data.local.entity.UserInfo
+import com.example.myapplication.model.remote.UserInfo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.net.HttpURLConnection
 
+/**
+ * 此類別提供登入服務
+ *
+ * @property status 狀態碼
+ *
+ * @author Ashley
+ */
 class LoginViewModel (private val userInfoDao: UserInfoDao) : ViewModel(){
     val allUser = userInfoDao.getAllUser()
 
+    private val _status = MutableLiveData<RemoteLoginStatus>(RemoteLoginStatus.Init)
+
+    /**
+     * 初始化為 Init 發送請求 loading 登入成功 Success 登入失敗 Failed
+     *
+     * @author Ashley
+     */
+    val status: LiveData<RemoteLoginStatus>
+        get() = _status
+
+    /**
+     * login 提供到後端的請求 此方法會改變 [status] 的狀態
+     *
+     * @param account 使用者帳號
+     * @param password 使用者密碼
+     *
+     * @author Ashley
+     */
+    fun login(account: String, password: String) {
+        _status.value = RemoteLoginStatus.Loading
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = RemoteClientManager.apiServiceClient.login(UserInfo(account, password))
+            if (result.status == HttpURLConnection.HTTP_OK) {
+                _status.postValue(RemoteLoginStatus.Success(account))
+            } else {
+                _status.postValue(RemoteLoginStatus.Failed(Throwable(result.message)))
+            }
+        }
+    }
+
     fun addUser(name : String , password : String){
         viewModelScope.launch(Dispatchers.IO){
-            userInfoDao.insert(UserInfo(0 ,name ,password))
+//            userInfoDao.insert(UserInfo(0 ,name ,password))
         }
     }
 
     fun deleteUser(userInfo: UserInfo){
         viewModelScope.launch(Dispatchers.IO) {
-            userInfoDao.delete(userInfo)
+//            userInfoDao.delete(userInfo)
         }
     }
 
+}
+
+/**
+ * 此類別提供狀態
+ */
+sealed class RemoteLoginStatus {
+    /**
+     * 初始化
+     */
+    object Init: RemoteLoginStatus()
+
+    /**
+     * 發送請求到取得結果之間的狀態
+     */
+    object Loading: RemoteLoginStatus()
+
+    /**
+     * 結果成功狀態
+     */
+    class Success(val account: String): RemoteLoginStatus()
+
+    /**
+     * 結果失敗狀態
+     */
+    class Failed(val throwable: Throwable): RemoteLoginStatus()
 }
